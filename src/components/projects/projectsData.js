ef5b3c36846ca028054d1425dc7db3cdf5bbcfcb
Danish0002 <121163878+Danish0002@users.noreply.github.com>
2025-07-17 10:40:30 +0530

    Update projectsData.js
    added LlmVerse

--- src/components/projects/projectsData.js
+++ src/components/projects/projectsData.js
@@ -17,6 +17,14 @@ const projects = [
     projectLink: "https://weather-app-two-beta-49.vercel.app/",
     image: "weatherAPP.png",
   },
+  {
+  title: "LlmVerse ",
+  description: "An intelligent chatbot platform powered by LangChain, supporting multiple AI providers like Mistral, Groq and Gemini via dynamic API switching.",
+  techStack: "React, Node.js, LangChain, Vercel, Mistral API, Groq API, OpenRouter API",
+  projectLink: "https://llm-verse.vercel.app/",
+  image: "LlmVerse.png",
+},
+
 ];
 
 export default projects;
